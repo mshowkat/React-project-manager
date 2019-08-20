1e4647e6972b3e5a8cac18c67e6ac5d3e12fc094
mshowkat <bangleblaze@gmail.com>
2019-08-20 21:15:43 +0600

    project reducer update

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,7 +9,14 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+            break;
     
+        default: console.log('default project')
+            break;
+    }
     return state
 }
 
